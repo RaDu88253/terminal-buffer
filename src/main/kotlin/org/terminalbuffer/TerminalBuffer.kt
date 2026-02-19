f9594fd83d0f4af8a2ccc0ab6b0d97205272e158
RaDu88253 <radupirvu09@gmail.com>
2026-02-19 02:10:20 +0200

    Cursor dependent manipulation and most content access features

--- src/main/kotlin/org/terminalbuffer/TerminalBuffer.kt
+++ src/main/kotlin/org/terminalbuffer/TerminalBuffer.kt
@@ -1,72 +1,79 @@
 package org.terminalbuffer
 
-class TerminalBuffer(val width: Int = 80, val height: Int = 24, val maximumScrollback: Int = 100) {
+class TerminalBuffer(
+    val width: Int = 80,
+    val height: Int = 24,
+    val maximumScrollback: Int = 100,
+    val tabSize: Int = 4
+) {
 
-    private val content = Array(height) {
-        Array(width) {Cell()}
+    private val screen = ArrayDeque<Array<Cell>>()
+    private val scrollback = ArrayDeque<Array<Cell>>()
+
+    init {
+        repeat(height) {
+            screen.addLast(Array(width) { Cell() })
+        }
     }
-    private val scrollback = Array(maximumScrollback){
-        Array(width) {Cell()}
-    }
-    private object Cursor{
+
+    private object Cursor {
         var x = 0
         var y = 0
-        var maxY = 0
     }
-    private object CurrentAttributes{
-        var foregroundColor = Colors.WHITE
-        var backgroundColor = Colors.BLACK
-        var style = 0
-    }
+
+    private var currentAttributes = Attributes()
 
     val styleMap = mapOf("BOLD" to (1 shl 0), "ITALIC" to (1 shl 1), "UNDERLINE" to (1 shl 2), "DEFAULT" to 0)
 
     fun setCurrentAttributes(
-        foregroundColor: Colors = CurrentAttributes.foregroundColor,
-        backgroundColor: Colors = CurrentAttributes.backgroundColor,
+        foregroundColor: Colors = currentAttributes.foregroundColor,
+        backgroundColor: Colors = currentAttributes.backgroundColor,
         vararg styles: String
     ) {
-        CurrentAttributes.foregroundColor = foregroundColor
-        CurrentAttributes.backgroundColor = backgroundColor
+        currentAttributes.foregroundColor = foregroundColor
+        currentAttributes.backgroundColor = backgroundColor
         if (!styles.isEmpty())
-            CurrentAttributes.style = 0;
-        for (style in styles){
-            CurrentAttributes.style = CurrentAttributes.style or styleMap.getOrDefault(style, 0)
+            currentAttributes.style = 0
+        for (style in styles) {
+            currentAttributes.style = currentAttributes.style or styleMap.getOrDefault(style, 0)
         }
     }
 
-    fun getForegroundColor() : Colors{
-        return CurrentAttributes.foregroundColor
+    fun getForegroundColor(): Colors {
+        return currentAttributes.foregroundColor
     }
 
-    fun getBackgroundColor() : Colors{
-        return CurrentAttributes.backgroundColor
+    fun getBackgroundColor(): Colors {
+        return currentAttributes.backgroundColor
     }
 
-    fun getStyle() : Int{
-        return CurrentAttributes.style
+    fun getStyle(): Int {
+        return currentAttributes.style
     }
 
-    fun getCursorPosition() : Pair<Int, Int>{
+    fun getCursorPosition(): Pair<Int, Int> {
         return Pair(Cursor.x, Cursor.y)
     }
 
-    fun setCursorPosition(x: Int, y: Int){
+    fun setCursorPosition(x: Int, y: Int) {
         Cursor.x = x.coerceIn(0, width - 1)
         Cursor.y = y.coerceIn(0, height - 1)
     }
 
-    fun moveCursor(direction: Directions, amount: Int = 1){
+    fun moveCursor(direction: Directions, amount: Int = 1) {
         when (direction) {
             Directions.RIGHT -> {
                 Cursor.x += amount
             }
+
             Directions.LEFT -> {
                 Cursor.x -= amount
             }
+
             Directions.DOWN -> {
                 Cursor.y += amount
             }
+
             Directions.UP -> {
                 Cursor.y -= amount
             }
@@ -77,25 +84,175 @@ class TerminalBuffer(val width: Int = 80, val height: Int = 24, val maximumScrol
 
     }
 
+    fun write(input: String) {
+        for (char in input) {
+            when (char) {
+                '\n' -> {
+                    screen[Cursor.y][Cursor.x].character = '\n'
+                    Cursor.y++
+                    wrapCursorUpdateScrollback()
+                }
+
+                '\r' -> Cursor.x = 0
+                '\b' -> {
+                    Cursor.x--
+                    wrapCursorUpdateScrollback()
+                    screen[Cursor.y][Cursor.x].character = ' '
+                }
+
+                '\t' -> {
+                    repeat(tabSize) {
+                        Cursor.x++
+                        wrapCursorUpdateScrollback()
+                    }
+                }
+
+                else -> {
+                    if (screen[Cursor.y][Cursor.x].character == '\n') {
+                        Cursor.x = 0
+                        Cursor.y++
+                        wrapCursorUpdateScrollback()
+                    }
+                    screen[Cursor.y][Cursor.x].character = char
+                    screen[Cursor.y][Cursor.x].attributes = currentAttributes.copy()
+                    Cursor.x++
+                    wrapCursorUpdateScrollback()
+                }
+            }
+        }
+    }
+
+    /**
+     * Inserts a text containing no line breaks at the current cursor position, wrapping lines if necessary
+     */
+    fun insertText(input: String) {
+
+        val cellsToRewrite = ArrayDeque<Cell>()
+        for (cell in screen[Cursor.y].slice(Cursor.x + 1 until width)) cellsToRewrite.addLast(cell.copy())
+        for (row in screen.slice(Cursor.y + 1 until height)){
+            for (cell in row) cellsToRewrite.addLast(cell.copy())
+        }
+
+        write(input)
+        val x = Cursor.x
+        val y = Cursor.y
+        writeFromCells(cellsToRewrite)
+        Cursor.x = x
+        Cursor.y = y
+        wrapCursorUpdateScrollback()
+    }
+
+    private fun writeFromCells(cells: ArrayDeque<Cell>) {
+        val initialAttributes = currentAttributes.copy()
+        while (cells.last().character == ' ')
+            cells.removeLast()
+        for (cell in cells){
+            if (cell.character != '\n') {
+                currentAttributes = cell.attributes.copy()
+                write(cell.character.toString())
+            }
+        }
+        currentAttributes = initialAttributes.copy()
+    }
+
+    fun fillCurrentLine(character: Char){
+        write(character.toString().repeat(width - Cursor.x))
+        wrapCursorUpdateScrollback()
+    }
+
+    fun wrapCursorUpdateScrollback() {
+        if (Cursor.x >= width) {
+            Cursor.x = 0
+            Cursor.y++
+        }
+        if (Cursor.y >= height) {
+            Cursor.y = height - 1
+            if (scrollback.size == maximumScrollback) {
+                scrollback.removeFirst()
+            }
+            scrollback.addLast(screen.first())
+            screen.removeFirst()
+            screen.addLast(Array(width) { Cell() })
+        }
+        if (Cursor.x < 0) {
+            Cursor.x = width - 1
+            Cursor.y--
+        }
+        if (Cursor.y < 0) {
+            Cursor.x = 0
+            Cursor.y = 0
+        }
+    }
+
+    fun getCharAtScreenPosition(x: Int, y: Int): Char {
+        if (y >= height) {
+            throw IndexOutOfBoundsException("Screen only has lines up to index $(height - 1)")
+        }
+        return screen[y][x].character
+    }
+
+    fun getAttributesAtScreenPosition(x: Int, y: Int): Attributes{
+        return screen[y][x].attributes
+    }
+
+    fun getCharAtScrollbackPosition(x: Int, y: Int): Char {
+        if (y >= scrollback.size) {
+            throw IndexOutOfBoundsException("Scrollback only has lines up to index $(scrollback.size - 1)")
+        }
+        return scrollback[y][x].character
+    }
+
+    fun getScreenContentAsString(): String{
+        val output =  screen.joinToString("") { row ->
+            buildString {
+                for (cell in row) append(cell.character)
+            }.trimEnd { it == ' ' }
+        }
+        return output
+    }
+
+    fun getScrollbackAndScreenContentAsString(): String{
+
+        //TODO: implement
+        return ""
+
+    }
+
+    fun getScreenRowAsString(y: Int): String {
+        if(y >= height)
+            throw IndexOutOfBoundsException()
+        val output = buildString {
+            for (cell in screen[y]) append(cell.character)
+        }.trimEnd { it == ' ' }
+        return output
+    }
+
+    fun getScrollbackRowAsString(y: Int): String {
+        if(y >= maximumScrollback)
+            throw IndexOutOfBoundsException()
+        val output = buildString {
+            for (cell in scrollback[y]) append(cell.character)
+        }.trimEnd { it == ' ' }
+        return output
+    }
+
 
 }
 
 private data class Cell(
-    val character: Char = ' ',
-    val charType: CharacterWidthType = CharacterWidthType.SIMPLE,
-    val foregroundColor: Colors = Colors.WHITE,
-    val backgroundColor: Colors = Colors.BLACK,
-    val style: Int = 0,
-    val lineStart: Boolean = false
+    var character: Char = ' ',
+    var charType: CharacterWidthType = CharacterWidthType.SIMPLE,
+    var attributes: Attributes = Attributes(),
+    var lineStart: Boolean = false
 )
 
-enum class CharacterWidthType{
+enum class CharacterWidthType {
     SIMPLE,
     WIDE_START,
     WIDE_END
 }
 
-enum class Colors{
+enum class Colors {
     RED,
     GREEN,
     YELLOW,
@@ -114,6 +271,10 @@ enum class Colors{
     LIGHTMAGENTA
 }
 
-enum class Directions{
+data class Attributes(var foregroundColor: Colors = Colors.WHITE,
+                      var backgroundColor: Colors = Colors.BLACK,
+                      var style: Int = 0)
+
+enum class Directions {
     UP, DOWN, LEFT, RIGHT
 }
---
+++ src/test/kotlin/org/terminalbuffer/WriteTest.kt
@@ -0,0 +1,194 @@
+package org.terminalbuffer
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class WriteTest {
+
+    @Test
+    fun `write function works in the general case`(){
+        val terminalBuffer = TerminalBuffer()
+        terminalBuffer.write("Test input\n")
+        val (x, y) = terminalBuffer.getCursorPosition()
+        val testChar = terminalBuffer.getCharAtScreenPosition(6, 0)
+        assertEquals(10, x)
+        assertEquals(1, y)
+        assertEquals('n', testChar)
+    }
+
+    @Test
+    fun `write function also updates the scrollback`(){
+        val terminalBuffer = TerminalBuffer()
+        for (i in 0 until 26){
+            terminalBuffer.write(i.toString() + "\n\r")
+        }
+        val testChar = terminalBuffer.getCharAtScrollbackPosition(0, 1)
+        assertEquals('1', testChar)
+    }
+
+    @Test
+    fun `backspace properly deletes characters`(){
+        val terminalBuffer = TerminalBuffer()
+        terminalBuffer.write("Te\b2st")
+        val testChar = terminalBuffer.getCharAtScreenPosition(1, 0)
+        assertEquals('2', testChar)
+    }
+
+    @Test
+    fun `cursor properly kept in bounds when trying to backspace out`(){
+        val terminalBuffer = TerminalBuffer()
+        terminalBuffer.write("Test\b\b\b\b\b\b")
+        val (x, y) = terminalBuffer.getCursorPosition()
+        assertEquals(0, x)
+        assertEquals(0, y)
+    }
+
+    @Test
+    fun `backspace properly goes to previous line`(){
+        val terminalBuffer = TerminalBuffer()
+        terminalBuffer.write("\n\n\b")
+        val (x, y) = terminalBuffer.getCursorPosition()
+        assertEquals(79, x)
+        assertEquals(1, y)
+    }
+
+    @Test
+    fun `carriage return works properly`(){
+        val terminalBuffer = TerminalBuffer()
+        terminalBuffer.write("Test\rAnotherTest")
+        val testChar = terminalBuffer.getCharAtScreenPosition(0, 0)
+        assertEquals('A', testChar)
+    }
+
+    @Test
+    fun `tabs properly move the cursor in the general case`(){
+        val terminalBuffer = TerminalBuffer()
+        terminalBuffer.write("\tTest")
+        val testChar = terminalBuffer.getCharAtScreenPosition(4, 0)
+        assertEquals('T', testChar)
+    }
+
+    @Test
+    fun `tabs properly move the cursor to the next line`(){
+        val terminalBuffer = TerminalBuffer()
+        terminalBuffer.write(CharArray(20){'\t'}.concatToString())
+//        val testChar = terminalBuffer.getCharAtScreenPosition(1, 1)
+//        assertEquals('T', testChar)
+        val (x, y) = terminalBuffer.getCursorPosition()
+        assertEquals(0, x)
+        assertEquals(1, y)
+
+    }
+
+    @Test
+    fun `text properly inserted in the general case`(){
+        val terminalBuffer = TerminalBuffer()
+        val input = "This is a test."
+        terminalBuffer.write(input)
+        terminalBuffer.setCursorPosition(9, 0)
+        terminalBuffer.insertText(" successful ")
+        val expectedOutput = "This is a successful test."
+        val testOutput = terminalBuffer.getScreenContentAsString()
+        assertEquals(expectedOutput, testOutput)
+    }
+
+    @Test
+    fun `text properly inserted on lines ending in a break`(){
+        val terminalBuffer = TerminalBuffer()
+        val input = "This is a test for lines ending in a line break.\n"
+        terminalBuffer.write(input)
+        terminalBuffer.setCursorPosition(9, 0)
+        val textToBeInserted = " successful "
+        terminalBuffer.insertText(textToBeInserted)
+        val (x, y) = terminalBuffer.getCursorPosition()
+        assertEquals(9 + textToBeInserted.length, x)
+        assertEquals(0, y)
+        val expectedOutput = "This is a successful test for lines ending in a \nline break."
+        val testOutput = terminalBuffer.getScreenContentAsString()
+        assertEquals(expectedOutput, testOutput)
+    }
+
+    @Test
+    fun `text properly inserted and wrapping the lines`(){
+        val terminalBuffer = TerminalBuffer(width = 20)
+        val input = "This is a test."
+        terminalBuffer.write(input)
+        terminalBuffer.setCursorPosition(9, 0)
+        terminalBuffer.insertText(" successful ")
+        val expectedScreen1stRow = "This is a successful"
+        val actualScreen1stRow = terminalBuffer.getScreenRowAsString(0)
+        assertEquals(expectedScreen1stRow, actualScreen1stRow)
+        val expectedScreen2ndRow = " test."
+        val actualScreen2ndRow = terminalBuffer.getScreenRowAsString(1)
+        assertEquals(expectedScreen2ndRow, actualScreen2ndRow)
+    }
+
+    @Test
+    fun `text properly inserted and updating the scrollback`(){
+        val terminalBuffer = TerminalBuffer(height = 2, width = 20)
+        val input = "This is a test."
+        terminalBuffer.write(input)
+        terminalBuffer.setCursorPosition(0, 1)
+        terminalBuffer.write(input)
+        terminalBuffer.setCursorPosition(9, 1)
+        terminalBuffer.insertText(" successful ")
+        val expectedScrollback1stRow = "This is a test."
+        val actualScrollback1stRow = terminalBuffer.getScrollbackRowAsString(0)
+        assertEquals(expectedScrollback1stRow, actualScrollback1stRow)
+    }
+
+    @Test
+    fun `fill line works properly`(){
+        val terminalBuffer = TerminalBuffer(width = 20)
+        val input = "This is a test."
+        terminalBuffer.write(input)
+        terminalBuffer.fillCurrentLine('*')
+        terminalBuffer.fillCurrentLine('*')
+        val expectedScreen1stRow = "This is a test.*****"
+        val actualScreen1stRow = terminalBuffer.getScreenRowAsString(0)
+        assertEquals(expectedScreen1stRow, actualScreen1stRow)
+        val expectedScreen2ndRow = "********************"
+        val actualScreen2ndRow = terminalBuffer.getScreenRowAsString(1)
+        assertEquals(expectedScreen2ndRow, actualScreen2ndRow)
+    }
+
+    @Test
+    fun `setting attributes works properly in the general case`(){
+        val terminalBuffer = TerminalBuffer(width = 20)
+        val input = "This is a test."
+        terminalBuffer.write(input)
+        terminalBuffer.setCurrentAttributes(
+            Colors.RED, Colors.GREEN, "BOLD", "ITALIC", "UNDERLINE"
+        )
+        terminalBuffer.fillCurrentLine('*')
+        val expectedAttributes = Attributes(Colors.RED, Colors.GREEN, 7)
+        val actualAttributes = terminalBuffer.getAttributesAtScreenPosition(16, 0)
+        assertEquals(expectedAttributes, actualAttributes)
+    }
+
+    @Test
+    fun `setting attributes works properly when inserting text`(){
+        val terminalBuffer = TerminalBuffer()
+        val input = "This is a test."
+        terminalBuffer.write(input)
+        terminalBuffer.setCurrentAttributes(
+            Colors.RED, Colors.GREEN, "BOLD", "ITALIC", "UNDERLINE"
+        )
+        terminalBuffer.setCursorPosition(9, 0)
+        terminalBuffer.insertText(" successful ")
+        val expectedAttributes = Attributes(Colors.RED, Colors.GREEN, 7)
+        val actualAttributes = terminalBuffer.getAttributesAtScreenPosition(9, 0)
+        assertEquals(expectedAttributes, actualAttributes)
+        val expected2ndRowAttributes = Attributes()
+        val actual2ndRowAttributes = terminalBuffer.getAttributesAtScreenPosition(0, 1)
+        assertEquals(expected2ndRowAttributes, actual2ndRowAttributes)
+    }
+
+    @AfterEach
+    fun resetTerminalBuffer(){
+        val terminalBuffer = TerminalBuffer()
+        terminalBuffer.setCursorPosition(0, 0)
+    }
+
+}
